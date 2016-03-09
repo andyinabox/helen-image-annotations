d834dc7bd420478f5db56e25df3e2442e931469f
Andy Dayton <amdayton@gmail.com>
2016-03-09 14:51:04 -0500

    Fixed image scaling

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var p5 = require('p5');
 var dat = require('exdat');
 
-var dataName = "2";
+var dataIndex = 1;
 var imageName = "";
 var annotations;
 var img;
@@ -9,7 +9,7 @@ var canvas;
 
 var sketch = function(p) {
 	p.preload = function() {
-		p.loadStrings('assets/annotations/'+dataName+".txt", parseData);
+		loadData(dataIndex);
 	}
 
 	p.setup = function() {
@@ -18,18 +18,54 @@ var sketch = function(p) {
 	}
 
 	p.draw = function() {
+		var imgWidth
+			, imgHeight;
+
+		p.background('#ddd');
+
 		if(img) {
-			p.image(img, 0, 0, p.width, p.height);
+
+			// rescale to fit canvas
+			if(img.width > img.height) {
+				imgWidth = p.width;
+				imgHeight = img.height*(p.width/img.width);
+			} else {
+				imgHeight = p.height;
+				imgWidth = img.width*(p.height/img.height);
+			}
+
+			p.translate((p.width-imgWidth)/2, (p.height-imgHeight)/2);
+
+			p.image(img, 0, 0, imgWidth, imgHeight);
 
 			annotations.forEach(function(coords) {
-				var x = p.map(coords.x, 0, img.width, 0, p.width);
-				var y = p.map(coords.y, 0, img.height, 0, p.height);
+				var x = p.map(coords.x, 0, img.width, 0, imgWidth);
+				var y = p.map(coords.y, 0, img.height, 0, imgHeight);
 				p.fill('yellow');
 				p.noStroke();
 				p.ellipse(x, y, 2, 2);
 			});
 		}
 
+		p.mouseClicked = function() {
+			if(p.mouseX > p.width/2) {
+				dataIndex = dataIndex+1;
+				console.log('load data index '+dataIndex);
+				loadData(dataIndex);
+			} else {
+				if(dataIndex-1 > 0) {
+					dataIndex = dataIndex-1;
+					loadData(dataIndex);
+				}
+			}
+		}
+
+	}
+
+	function loadData(i) {
+		var url = 'assets/annotations/'+i+".txt";
+		console.log('load data', url);
+		p.loadStrings(url, parseData);		
 	}
 
 	function parseData(data) {
