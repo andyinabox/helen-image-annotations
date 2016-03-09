7826ce058a5e06263ba01a6b6bf01ca8a57bf315
Andy Dayton <amdayton@gmail.com>
2016-03-09 15:13:37 -0500

    Added additional controls

--- index.js
+++ index.js
@@ -1,20 +1,39 @@
 var p5 = require('p5');
 var dat = require('exdat');
 
-var dataIndex = 1;
+// var dataIndex = 1;
 var imageName = "";
 var annotations;
 var img;
 var canvas;
 
+var gui;
+var params = {
+	showImage: true
+	, showAnnotations: true
+	, annotationSize: 3
+	, dotColor: [255, 255, 255]
+	, dataIndex: 1
+}
+
 var sketch = function(p) {
 	p.preload = function() {
-		loadData(dataIndex);
+		loadData();
 	}
 
 	p.setup = function() {
 		var size = 600; //p.min(p.width, p.height);
 		canvas = p.createCanvas(p.windowWidth, p.windowHeight);
+
+		gui = new dat.GUI();
+
+		gui.add(params, 'showImage');
+		gui.add(params, 'showAnnotations');
+		gui.add(params, 'annotationSize');
+		gui.addColor(params, 'dotColor');
+		gui.add(params, 'dataIndex')
+			.step(1)
+			.onChange(loadData);
 	}
 
 	p.draw = function() {
@@ -36,27 +55,30 @@ var sketch = function(p) {
 
 			p.translate((p.width-imgWidth)/2, (p.height-imgHeight)/2);
 
-			p.image(img, 0, 0, imgWidth, imgHeight);
+			if(params.showImage) {
+				p.image(img, 0, 0, imgWidth, imgHeight);
+			}
 
-			annotations.forEach(function(coords) {
-				var x = p.map(coords.x, 0, img.width, 0, imgWidth);
-				var y = p.map(coords.y, 0, img.height, 0, imgHeight);
-				p.fill('yellow');
-				p.noStroke();
-				p.ellipse(x, y, 2, 2);
-			});
+			if(params.showAnnotations) {
+				annotations.forEach(function(coords) {
+					var x = p.map(coords.x, 0, img.width, 0, imgWidth);
+					var y = p.map(coords.y, 0, img.height, 0, imgHeight);
+					p.fill(params.dotColor);
+					p.noStroke();
+					p.ellipse(x, y, params.annotationSize, params.annotationSize);
+				});
+			}
 		}
 	}
 
-	p.mouseClicked = function() {
-		if(p.mouseX > p.width/2) {
-			dataIndex = dataIndex+1;
-			console.log('load data index '+dataIndex);
-			loadData(dataIndex);
-		} else {
-			if(dataIndex-1 > 0) {
-				dataIndex = dataIndex-1;
-				loadData(dataIndex);
+	p.keyTyped = function() {
+		if(p.key === 'k') {
+			params.dataIndex = params.dataIndex+1;
+			loadData();
+		} else if(p.key === 'j') {
+			if(params.dataIndex-1 > 0) {
+				params.dataIndex = params.dataIndex-1;
+				loadData();
 			}
 		}
 	}
@@ -68,7 +90,8 @@ var sketch = function(p) {
 
 
 	function loadData(i) {
-		var url = 'assets/annotations/'+i+".txt";
+		var index = i || params.dataIndex;
+		var url = 'assets/annotations/'+index+".txt";
 		console.log('load data', url);
 		p.loadStrings(url, parseData);		
 	}
