0df4cb206f6fe860f72b88902d785e4c0ccb413a
Andy Dayton <amdayton@gmail.com>
2016-04-22 11:16:47 -0400

    Seems like aws parse script is working

--- scripts/parse.js
+++ scripts/parse.js
@@ -1,23 +1,34 @@
-var fs = require('fs')
+var AWS = require('aws-sdk')	
+	, fs = require('fs')
 	, path = require('path')
-	, sizeOf = require('image-size');
+	// , sizeOf = require('image-size')
+	, lwip = require('lwip')
 
+	, s3 = new AWS.S3();
 
-var dir = path.resolve(__dirname, '../assets/annotations');
+var HELEN_BUCKET = 'helen-images';
+var AWS_IMAGES_PATH = 'images/';
+var ANNOTATIONS_DIR = path.resolve(__dirname, '../assets/annotations');
 
 var out = [];
 var csv = '';
 
-fs.readdir(dir, function(err, files) {
+fs.readdir(ANNOTATIONS_DIR, function(err, files) {
 	if(err) throw err;
 
-	files.forEach(function(f) {
-		var basename = path.basename(f, '.txt');
-		var data = fs.readFileSync(path.resolve(dir, f), 'utf8')
+	var remaining = 10;
+
+	next(0);
+
+	function next(index) {
+		var f = files[index]
+			, basename = path.basename(f, '.txt')
+			, data = fs.readFileSync(path.resolve(ANNOTATIONS_DIR, f), 'utf8')
 			, index = parseInt(basename)
 			, lines = data.toString().split("\n")
 			, imageName = lines.shift().replace(/\s/g, '') + '.jpg'
-			, imagePath = path.resolve(__dirname, '../assets/images/'+imageName)
+			// , imagePath = path.resolve(__dirname, '../assets/images/'+imageName)
+			, imagePath = AWS_IMAGES_PATH + imageName
 			, imageSize
 			, image = [imageName, null, null]
 			, annotations;
@@ -26,14 +37,6 @@ fs.readdir(dir, function(err, files) {
 		// remove empty line at end
 		lines.pop();
 
-		if(fs.existsSync(imagePath)) {
-			imageSize = sizeOf(imagePath);
-			image[1] = imageSize.width;
-			image[2] = imageSize.height;
-		}
-
-		console.log(imagePath, imageSize);
-
 		// map annotations
 		annotations = lines.map(function(line) {
 			var parts = line.split(',');
@@ -41,17 +44,69 @@ fs.readdir(dir, function(err, files) {
 		}); 
 
 		out[index] = [image, annotations];
-	});
 
-	// remove null at beginning
-	out.shift();
+		var getObjectParams = {
+			Bucket: HELEN_BUCKET
+			, Key: imagePath
+		}
 
-	csv = out.map(function(d, i) {
-		return i + ',' + d[0].join(',');
-	}).join("\n");
+		console.log('attempt to get '+getObjectParams.Key);
 
-	// write file
-	fs.writeFile(path.resolve(__dirname, '../assets/annotations.json'), JSON.stringify(out), 'utf8');
-	fs.writeFile(path.resolve(__dirname, '../assets/helen.csv'), csv, 'utf8');
-	
+		s3.getObject(getObjectParams, function(err, data) {
+			if(err) {
+				checkDone(err, index);
+			} else {
+				console.log('got response for '+getObjectParams.Key);
+
+				lwip.open(data.Body, "jpg", function(err, imgObj) {
+					if(err) {
+						checkDone(err, index);
+					} else {
+						// set height/width
+						image[1] = imgObj.width();
+						image[2] = imgObj.height();
+						console.log('updated image', image);
+						checkDone(null, index);
+					}
+				});
+			}
+		});
+
+
+		// if(fs.existsSync(imagePath)) {
+		// 	imageSize = sizeOf(imagePath);
+		// 	image[1] = imageSize.width;
+		// 	image[2] = imageSize.height;
+		// }
+
+		// console.log(imagePath, imageSize);
+
+
+	};
+
+	function checkDone(err, currentIndex) {
+		if(err) console.log(err);
+
+		remaining--;
+
+		if(remaining <= 0) {
+			done();
+		} else {
+			next(currentIndex+1);
+		}
+	}
+
+
+	function done() {
+		// remove null at beginning
+		out.shift();
+
+		csv = out.map(function(d, i) {
+			return i + ',' + d[0].join(',');
+		}).join("\n");
+
+		// write file
+		fs.writeFile(path.resolve(__dirname, '../assets/annotations.json'), JSON.stringify(out), 'utf8');
+		fs.writeFile(path.resolve(__dirname, '../assets/helen.csv'), csv, 'utf8');
+	}
 });
